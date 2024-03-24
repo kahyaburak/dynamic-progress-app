415ca2d6dca1073ed294a3c35e2a08e6a63739ba
Burak <burakkahyabk@gmail.com>
2024-03-24 18:03:34 +0300

    finalized deployment

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ const express = require('express');
 const db = require('./data/database');
 const mainRoutes = require('./routes/main.routes');
 
+let port = 3000;
+if (process.env.PORT) {
+  port = process.env.PORT;
+}
+
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -15,13 +20,13 @@ app.use(express.static('public'));
 
 app.use(mainRoutes);
 
-app.use(function(error, req, res, next) {
+app.use(function (error, req, res, next) {
   res.status(500).render('500');
-})
+});
 
 db.initDatabase()
   .then(function () {
-    app.listen(3000);
+    app.listen(port);
   })
   .catch(function (error) {
     console.log('Connecting to the database failed!');
